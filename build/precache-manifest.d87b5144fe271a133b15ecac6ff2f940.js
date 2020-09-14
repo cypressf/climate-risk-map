40d2632581f1865c61543065c13e7e642e72a61a
Cypress Frankenfeld <iamcypress@gmail.com>
2020-09-14 17:53:34 -0400

    Add build folder

---
+++ build/precache-manifest.d87b5144fe271a133b15ecac6ff2f940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ac56297f9a15a9688e6d2ee3246902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6780bc0f0ca6402dcc86",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c806c3f8ef906bb35bc",
+    "url": "/static/js/2.43b85372.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43b85372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6780bc0f0ca6402dcc86",
+    "url": "/static/js/main.4fbeb420.chunk.js"
+  },
+  {
+    "revision": "8df10219b601660c6658",
+    "url": "/static/js/runtime-main.f657013a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
